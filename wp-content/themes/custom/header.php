a4e26f5a4af03228e57ea88afb7d5724ba7de059
DESKTOP-E5OKJQ8\Lenovo <artimizia.dias@kilowott.com>
2024-05-08 18:03:55 +0530

    nav-walker

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -4,6 +4,7 @@
 	<meta charset="<?php bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width">
 	<?php $logo=get_field('logo','option');
+	$customWalker = new Menu_With_Description;
 	if($logo):?>
 	 <img src="<?php echo $logo?>" style="width:50px;height:50px;margin-bottom: -40px;"> 
 	<?php endif;
@@ -16,6 +17,7 @@
 		<h1><?php bloginfo('name'); ?><h1>
 		<h5><?php bloginfo('description');?></h5>
 		<nav class="site-nav">
-			<?php $args=array('theme_location'=>'primary');?>
+			<?php $args=array('theme_location'=>'primary','walker'=>$customWalker);?>
 			<?php wp_nav_menu($args);?>
-	</header>
+	</header
+
