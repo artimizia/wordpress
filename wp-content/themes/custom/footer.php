fe9fa5448639592db80e3d50b5b0e7a37bdca39f
DESKTOP-E5OKJQ8\Lenovo <artimizia.dias@kilowott.com>
2024-05-08 18:08:02 +0530

    footer

--- wp-content/themes/custom/footer.php
+++ wp-content/themes/custom/footer.php
@@ -5,9 +5,7 @@
 	<p><?php bloginfo("name");?>-&copy;<?php echo date('y')?>
 
 	<nav class="site-nav">
-			<?php $args=array('theme_location'=>'primary');?>
-			<?php wp_nav_menu($args);?>
-			<?php $args=array('theme_location'=>'footer');?>
+			<?php $customWalker = new Menu_With_Description; $args=array('theme_location'=>'footer','walker'=>$customWalker);?>
 			<?php wp_nav_menu($args);?>
     </nav >
 			
